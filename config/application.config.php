ddfd1db30e6cb0e752fcecb13a36736ac0dedccb
Ilya Pleshakov <da-eto@ya.ru>
2014-04-15 03:31:14 +0400

    add Twig

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return [
     // This should be an array of module namespaces used in the application.
     'modules' => [
         'Application',
+        'ZfcTwig',
     ],
 
     // These are various options for the listeners attached to the ModuleManager
