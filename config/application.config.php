f44a0e94ca57f27741f273c155362684ec8187ec
Ilya Pleshakov <da-eto@ya.ru>
2014-04-15 03:47:02 +0400

    add Developer Tools

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,10 @@
 return [
     // This should be an array of module namespaces used in the application.
     'modules' => [
-        'Application',
+        'ZendDeveloperTools',
+        'OcraServiceManager',
         'ZfcTwig',
+        'Application',
     ],
 
     // These are various options for the listeners attached to the ModuleManager
